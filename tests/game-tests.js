ca3c4fed585a5031e4fd5eed6f6859e25e0a71c8
John Montgomery <john@littlespikeyland.com>
2011-02-27 19:32:13 +0000

    Few sprite related tests

--- tests/game-tests.js
+++ tests/game-tests.js
@@ -14,23 +14,30 @@ test("world has expected methods", function() {
     ok(world.checkCollisions !== undefined, "checkCollisions method present");
     ok(world.sprite !== undefined, "sprite method present");
 });
-test("world create layer", function(){
+test("world create layer", function() {
     var world_elem = $('<div>');
     var world = GameJS.world(world_elem);
     var layer = world.layer('main-layer');
     ok(layer !== undefined, "layer returned");
     same(world.layer('main-layer'), layer, "layer not re-created");
 });
+test("world create sprite", function() {
+    var world_elem = $('<div>');
+    var world = GameJS.world(world_elem);
+    var sprite = world.sprite('main-sprite');
+    ok(sprite !== undefined, "sprite returned");
+    same(world.sprite('main-sprite'), sprite, "sprite not re-created");
+})
 
 module("Layer")
-test("layer has expected methods", function(){
+test("layer has expected methods", function() {
     var world_elem = $('<div>');
     var world = GameJS.world(world_elem);
     var layer = world.layer('main-layer');
     ok(layer.add !== undefined, "add method present");
     ok(layer.elem !== undefined, "add method present");
 });
-test("layer backed by div", function(){
+test("layer backed by div", function() {
     var world_elem = $('<div>');
     var world = GameJS.world(world_elem);
     var layer = world.layer('main-layer');
@@ -39,12 +46,37 @@ test("layer backed by div", function(){
     ok(layer_elem.hasClass('layer'), "correct layer class");
     ok(layer_elem.get(0).nodeName.toLowerCase(), "div", "layer is div");
 });
-test("layer div in world", function(){
+test("layer div in world", function() {
     var world_elem = $('<div>');
     var world = GameJS.world(world_elem);
     var layer = world.layer('main-layer');
     equals(world_elem.find('#main-layer').size(), 1,
            "world element contains layer div");
 });
+test("layer add sprite", function() {
+    var world_elem = $('<div>');
+    var world = GameJS.world(world_elem);
+    var layer = world.layer('main-layer');
+    var sprite = world.sprite('main-sprite');
+    
+    equals(world_elem.find('#main-layer > #main-sprite').size(), 0,
+           "no sprite initially");
+    
+    layer.add(sprite);
+    
+    equals(world_elem.find('#main-layer > #main-sprite').size(), 1,
+          "sprite in layer after adding");
 
+});
+
+module("Sprite")
+test("sprite backed by div", function() {
+    var world_elem = $('<div>');
+    var world = GameJS.world(world_elem);
+    var sprite = world.sprite('main-sprite');
+    var sprite_elem = sprite.elem();
+    equals(sprite_elem.attr('id'), 'main-sprite', "correct sprite id");
+    ok(sprite_elem.hasClass('sprite'), "correct sprite class");
+    ok(sprite_elem.get(0).nodeName.toLowerCase(), "div", "sprite is div");
+});
 
